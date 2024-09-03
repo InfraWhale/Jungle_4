3be2e24ec301ac82eec95b7e987689d43a56f122
Ubuntu <ubuntu@ip-172-31-14-201.ap-northeast-2.compute.internal>
2024-09-03 05:17:21 +0000

    find_erase_rand

--- src/driver.c
+++ src/driver.c
@@ -1,25 +1,5 @@
 #include "rbtree.h"
 
-// #include <stdio.h> // 추후삭제
-
 int main(int argc, char *argv[]) {
-
-// int key = 128;
-
-// rbtree *t = new_rbtree();
-// node_t *p = rbtree_insert(t, key);
-
-// printf("root : %d \n", t->root->key);
-// printf("p : %d \n", p->key);
-
-// rbtree_erase(t, p);
-
-// printf("p : %d \n", p->key);
-
-// if (t->root == t->nil) {
-// printf("good \n");
-// }
-
-// delete_rbtree(t);
 
 }
